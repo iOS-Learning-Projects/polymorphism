942eb7a7f48228dceca45ee9394fa27f7fbe0051
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-02-12 14:07:50 -0200

    just add

---
+++ Veiculo.swift
@@ -0,0 +1,13 @@
+//
+//  Veiculo.swift
+//  Polimorfismo
+//
+//  Created by Eduardo Vital Alencar Cunha on 20/03/17.
+//  Copyright © 2017 Vital. All rights reserved.
+//
+
+import Cocoa
+
+class Veiculo: NSObject {
+
+}
